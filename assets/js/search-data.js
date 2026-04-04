b0ce1c507cdee48452cf8d950301755548ba4c95
w4iei <jaffe@gatech.edu>
2026-04-04 12:22:46 +0000

    Deploying to gh-pages from @ w4iei/noahj_website@0dfcd423a2fca52da051f3d5e119e5e15e61a174 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,30 +23,4 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+        },];
